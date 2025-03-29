c79d456c6e938fff9cf5ba9360efbabd3291d50c
gokce-tezel-avsar <gokcetezelavsar@gmail.com>
2025-03-29 22:46:15 +0300

    home tamammmm

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import AciktimBtn from "../components/AciktimBtn";
 import "./home.css";
-import { useEffect } from "react";
+import { useEffect,Image } from "react";
+
+
 
 function Home() {
-  useEffect(() => {
-    document.body.classList.add("home-bg");
-    
-    return () => {
-      document.body.classList.remove("home-bg"); 
-    };
-  }, []);
+
   return (
-    <div className="home">
-      <img className="home-logo-img" src="/images/iteration-1-images/logo.svg" alt="logo" />
+   
+    <div className="home-bg"> 
+     <img  className="bg-image" src="/images/iteration-1-images/home-banner.png" alt="bg-image" />
+    <div className="logo">
+      <img src="/images/iteration-1-images/logo.svg" alt="logo" />
+      </div>
       <div className="home-paragraf">
         <h1>KOD ACIKTIRIR</h1>
         <h1>PİZZA, DOYURUR</h1>
